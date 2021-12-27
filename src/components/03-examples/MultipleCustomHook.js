e7112263a2517182c41e6ca17e8c65ea2319b100
Canos13 <canomsergio13@gmail.com>
2021-12-27 17:41:48 -0600

    miltiple custom Hook

--- src/components/03-examples/MultipleCustomHook.js
+++ src/components/03-examples/MultipleCustomHook.js
@@ -1,16 +1,51 @@
 import React from 'react';
+import { Loading } from './Loading';
+import { useCounter } from '../../Hooks/useCounter';
 import { useFetch } from '../../Hooks/useFetch';
 import '../02-useEffect/Form.css'
 
 export const MultipleCustomHook = () => {
 
-    const url = 'https://www.breakingbadapi.com/api/quotes/1';
+    const {state, increment, decrement, reset} = useCounter(1);
+    if(state===0){
+        reset();
+    }
+    const url = `https://www.breakingbadapi.com/api/quotes/${state}`;
+    const {loading, data} = useFetch(url);
+    const { author, quote } = !!data && data[0];
 
-    const state = useFetch(url);
-    console.log(state);
     return (
         <>
-            <h1>MultipleCustomHook</h1>
+            <h1 className='text-center'>BreakingBad Quotes</h1>
+            <hr/>
+            {
+                loading 
+                ? 
+                   (
+                        <Loading />
+                   )
+                :
+                    (
+                        <blockquote className='blockquote text-center'>
+                            <p className='mb-3'>{ author }</p>
+                            <footer className='blockquote-footer'>{ quote }</footer>
+                        </blockquote>
+                    )
+            }
+
+            <div className='w-100 d-flex justify-content-between'>
+                <button 
+                    className='btn btn-success m-1'
+                    onClick={decrement}
+                >Anterior Quote
+                </button>
+
+                <button 
+                    className='btn btn-success m-1'
+                    onClick={increment}
+                >Siguiente Quote
+                </button>
+            </div>
         </>
     )
 }
