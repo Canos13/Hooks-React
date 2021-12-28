6d16026c9bbdd3b188057bd62d83e5b9fe27b7cd
Canos13 <canomsergio13@gmail.com>
2021-12-28 16:22:32 -0600

    CallbackHook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Memorize } from './components/05-Memos/Memorize';
+import { CallbackHook } from './components/05-Memos/CallbackHook';
 /* import { HooksApp } from './HooksApp' 
+import { Memorize } from './components/05-Memos/Memorize';
 import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 import { MultipleCustomHook } from './components/03-examples/MultipleCustomHook';
 import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
@@ -10,6 +11,6 @@ import { CounterAppCustomHook } from './components/01-useSate/CounterAppCustomHo
 import { SimpleForm } from './components/02-useEffect/SimpleForm';*/
 
 ReactDOM.render(
-  <Memorize />, document.getElementById('root')
+  <CallbackHook />, document.getElementById('root')
 );
 
