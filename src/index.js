2673ffcd73cacd6799bc4fac678869cdc03643e2
Canos13 <canomsergio13@gmail.com>
2021-12-28 17:02:07 -0600

    uso del Componente Padre

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CallbackHook } from './components/05-Memos/CallbackHook';
+import { Padre } from './components/06-tarea-memo/Padre';
 /* import { HooksApp } from './HooksApp' 
 import { Memorize } from './components/05-Memos/Memorize';
+import { CallbackHook } from './components/05-Memos/CallbackHook';
 import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 import { MultipleCustomHook } from './components/03-examples/MultipleCustomHook';
 import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
@@ -11,6 +12,6 @@ import { CounterAppCustomHook } from './components/01-useSate/CounterAppCustomHo
 import { SimpleForm } from './components/02-useEffect/SimpleForm';*/
 
 ReactDOM.render(
-  <CallbackHook />, document.getElementById('root')
+  <Padre />, document.getElementById('root')
 );
 
