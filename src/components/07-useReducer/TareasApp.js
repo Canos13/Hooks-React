4dd292693f8d97ac7c1b16835d89c79560457049
Canos13 <canomsergio13@gmail.com>
2022-01-02 22:54:38 -0600

    borrar comentario

--- src/components/07-useReducer/TareasApp.js
+++ src/components/07-useReducer/TareasApp.js
@@ -79,33 +79,6 @@ export const TareasApp = () => {
                         handleDelete={handleDelete}
                         handleToggle={handleToggle}
                     />
-
-                    {/* <ol className='list-group list-group-flush' >
-                        {
-                            tareas.map(({desc, id, done},i) => {
-                                return <li
-                                            key={id}
-                                            className='list-group-item d-flex flex-wrap align-items-center justify-content-between'
-                                        >
-                                            <p 
-                                                className={`col-6 hand flex-wrap m-0 ${done && 'complete'}`} 
-                                                onClick={()=> handleToggle(id)}
-                                            >
-                                                    {i+1}. {desc}
-                                            </p>
-                                            <button
-                                                className={`btn btn-${done?'secondary':'primary'}`}
-                                                onClick={ ()=> handleToggle(id)}
-                                            ><i className={`${done?'fas fa-window-close':'far fa-check-square'}`}></i> {`${done?'Incompleto':'Hecho'}`}</button>
-                                            
-                                            <button
-                                                className='btn btn-danger'
-                                                onClick={ () => handleDelete(id) }
-                                            ><i className="far fa-trash-alt"></i> Borrar</button> 
-                                        </li>
-                            })
-                        }
-                    </ol> */}
                     
                 </div>
 
