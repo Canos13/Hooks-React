0dcc7f75375d107c23030ff92e1321fa6406b68a
Canos13 <canomsergio13@gmail.com>
2022-02-22 19:36:03 -0600

    hacemos uso del useContext

--- src/components/09-useContext/HomeScreen.js
+++ src/components/09-useContext/HomeScreen.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const HomeScreen = () => {
+  const { user } = useContext( UserContext );
+
+  console.log(user);
+
   return (
     <>
         <h1 className='text-center mt-3'>HomeScreen</h1>
+
+        <pre className='d-inline-block mx-auto ' >
+            { JSON.stringify(user, null, 3) }
+        </pre>
     </>
   )
 }
