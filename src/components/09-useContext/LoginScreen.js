cb7c458295675f7f7993e161ea6d9935e83ec8fc
Canos13 <canomsergio13@gmail.com>
2022-02-22 19:36:36 -0600

    agregamos el boton y usamos el useContext

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import { UserContext } from './UserContext';
 
 export const LoginScreen = () => {
+  const { setUser } = useContext( UserContext );
+  
   return (
     <>
         <h1 className='text-center mt-3'>LoginScreen</h1>
+
+        <button
+          className='btn btn-danger mx-auto d-block'
+          onClick={ ()=>setUser({
+              id: 13,
+              name: 'Sergio',
+              lastName: 'Cano'
+          }) }
+        >login</button>
     </>
   )
 }
